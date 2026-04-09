504bf3b5cf0b579633c0c2441527695a6a9eb957
Eugene Tan <neko.temp93@gmail.com>
2026-04-09 11:48:00 +0800

    second commit

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -1,7 +1,7 @@
 import Database from 'better-sqlite3'
 import path from 'path'
 
-const DB_PATH = path.join(process.cwd(), 'todos.db')
+const DB_PATH = path.join(process.env.RAILWAY_VOLUME_MOUNT_PATH || process.cwd(), 'todos.db')
 
 let _db: Database.Database | null = null
 
